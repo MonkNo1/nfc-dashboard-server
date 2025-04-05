4457e747ecccdee19b33eefc36f3766076a6802d
Santhosh <sbuvanes@utk.edu>
2025-04-05 14:38:48 +0530

    Update Appointmentt.js

--- server/models/Appointment.js
+++ server/models/Appointment.js
@@ -1,5 +1,11 @@
-const mongoose = require('mongoose');
-const AppointmentSchema = new mongoose.Schema({ ... });
+import mongoose from 'mongoose';
+
+const AppointmentSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  date: { type: String, required: true },
+  time: { type: String, required: true },
+}, { timestamps: true });
 
 const Appointment = mongoose.model('Appointment', AppointmentSchema);
 export default Appointment;
