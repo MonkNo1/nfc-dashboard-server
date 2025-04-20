5e79066a854cbcea7fd2b870ba2e36df3577d0f3
Santhosh <sbuvanes@utk.edu>
2025-04-20 17:47:45 +0530

    Update auth.js

--- server/routes/v1/auth.js
+++ server/routes/v1/auth.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { google } from 'googleapis';
+import { OAuth2Client } from 'google-auth-library';
 import jwt from 'jsonwebtoken';
 import User from '../../models/User.js';
 
 const router = express.Router();
-const oauth2Client = new google.auth.OAuth2(
+const oauth2Client = new OAuth2Client(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
   process.env.GOOGLE_REDIRECT_URI
