3e23de710f35718754d2508d7d95334e44e12f75
Santhosh <sbuvanes@utk.edu>
2025-04-21 23:15:42 +0530

    Env Variable mismatch fix

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const mongoURI = process.env.MONGO_URI;
+    const mongoURI = process.env.MONGODB_URI;
     
     if (!mongoURI) {
       throw new Error('MongoDB URI is not defined in environment variables');
